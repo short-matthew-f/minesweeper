60cb58629da5669dfc59f3cba1bb4d70d5e4ce49
Constance and Matthew <constance+matthew@appacademy.io>
2014-05-12 11:23:46 -0400

    initialize new_board, add skeleton of Tile class

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,7 +1,56 @@
 class Minesweeper
-  def initialize(args)
+  TOTAL_MINES = 10
+  
+  # factory methods
+  def self.new_board
+    board = (0..8).map do |row|
+      (0..8).map do |tile|
+        Tile.new
+      end
+    end
     
+    board.flatten.shuffle.take(10).map do |t|
+      t.set_bomb
+    end
+    
+    board
+  end  
+  
+  # public methods
+  def initialize(board = nil)
+    @board = board || Minesweeper.new_board
   end
   
+  # private methods
   
+  private
+  
+  # protected methods
+  
+  protected
+  
+  
+end
+
+class Tile
+  attr_reader :flagged, :bomb, :fringe
+  
+  def initialize
+  end
+  
+  def bomb?
+    bomb
+  end
+  
+  def flagged?
+    flagged
+  end
+  
+  def fringe?
+    fringe
+  end
+  
+  def set_bomb
+    bomb = true
+  end
 end
